700df7bc8f1ea4fccce9e1f55559bcb04c6a2bbc
Jia XU <xujia118@hotmail.com>
2024-04-25 07:27:56 -0700

    Added a comparison of both approaches

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,25 @@ import { createContext, useReducer } from "react";
 
 import reducer, { initialState } from "./reducer";
 
+// import { updateUsername } from "./utils";
 import ChildB from "./ChildB";
-import { updateUsername } from "./utils";
 
 export const userContext = createContext();
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  function updateUsername(username) {
+    dispatch({ type: 'setName', username })
+  }
+
   return (
     <div>
       <h2>{`Hello ${state.username} from A`}</h2>
       <userContext.Provider
         value={{
-          updateUsername: updateUsername(dispatch),
+          // updateUsername: updateUsername(dispatch),
+          updateUsername,
           username: state.username,
         }}
       >
--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,8 @@ export function updateUsername(dispatch) {
         dispatch({ type: "setName", username})
     }
 }
+
+// Can also use the following arrow function
+// export const updateUsername = (dispatch) => (username) => {
+//   dispatch({ type: "setName", username });
+// };
